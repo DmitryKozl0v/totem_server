7409ec6d572aa75edf9da2b56f54a0aaad227f23
DmitryKozl0v <gonzalonvaldez@gmail.com>
2021-04-28 10:20:01 -0300

    Changes for bands app done

--- sockets/socket.js
+++ sockets/socket.js
@@ -1,11 +1,23 @@
 const {io} = require('../index');
+const Band = require('../models/band');
+const Bands = require('../models/bands');
+
+const bands = new Bands();
+
+bands.addBand(new Band('Periphery'));
+bands.addBand(new Band('Tesseract'));
+bands.addBand(new Band('Polyphia'));
+bands.addBand(new Band('CHON'));
+bands.addBand(new Band('Sleep Token'));
+
 
 // Sockets messages
 io.on('connection', client => {
     console.log('Client logged in');
 
-    client.on('disconnect', () => { 
+    client.emit('active-bands', bands.getBands());
 
+    client.on('disconnect', () => {
         console.log('Client logged out')
     });
 
@@ -13,6 +25,43 @@ io.on('connection', client => {
 
         console.log('Payload: ', payload)
         io.emit('message', {admin: 'New message!'})
+    });
+
+    client.on('new-message', (payload) => { 
+
+        io.emit('new-message', payload); // emits to every client
+        client.broadcast.emit('new-message', payload); // emits to every client except the one emiting
+    });
+
+    client.on('message-from-client', (payload) =>{
+        
+        console.log('New message:');
+        console.log('From: ' + payload['name']);
+        console.log('Message: ' + payload['message']);
+        console.log('.===========================.');
+        io.emit('message-from-client', payload);
+    });
+
+    client.on('submit-vote', (payload) => {
+
+        bands.voteBand(payload.id);
+        io.emit('active-bands', bands.getBands());
+
+    });
+
+    client.on('add-band', (payload) => {
+
+        const newBand = new Band(payload['name'])
+        bands.addBand(newBand);
+        io.emit('active-bands', bands.getBands());
+
+    });
+
+    client.on('delete-band', (payload) => {
+
+        bands.deleteBand(payload['id']);
+        io.emit('active-bands', bands.getBands());
+
     });
 
 });
